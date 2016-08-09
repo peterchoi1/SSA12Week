3a1fe4ea9ae5c74cf50475c2fdd039f75249673b
Peter <peterchoi1123@gmail.com>
2016-08-08 20:13:37 -0400

    Week Two - Assignment One

--- src/main/java/com/tiy/ssa/weektwo/assignmentone/Lists.java
+++ src/main/java/com/tiy/ssa/weektwo/assignmentone/Lists.java
@@ -1,23 +1,34 @@
 package com.tiy.ssa.weektwo.assignmentone;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Lists {
 
-    public static <T> List<T> match(List<T> one, List<T> other) {
-        
-        
-        
-        
-        
+    public static List<Food> match(List<Food> one, List<Food> other) {
+
+        List<Food> foodList = new ArrayList();
+
+        Iterator<Food> listOne = one.iterator();
+
+        while (listOne.hasNext()) {
+            Food food = listOne.next();
+
+            if (other.contains(food)) {
+                foodList.add(food);
+            }
+
+        }
+
+        return foodList;
+
     }
-    
+
     public static List<String> descendingBySize(List<String> input) {
-        
-        
-        
-        
-        
+        Collections.sort(input, Collections.reverseOrder());
+        return input;
     }
-    
-    
-    
+
 }
